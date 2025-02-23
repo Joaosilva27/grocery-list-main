cb68977718d0ccab3591b4025de0108a9c34e554
João Silva <joaosilva7875@gmail.com>
2025-02-23 21:04:38 +0100

    css for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -196,23 +196,25 @@ function App() {
             </div>
           </>
         ) : (
-          <div className="text-center p-8 rounded-lg bg-gray-800">
-            <h1 className="text-2xl text-white mb-4">Grocery List App</h1>
-            <button
-              onClick={signInWithGoogle}
-              className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-xl text-lg flex items-center justify-center gap-2"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
+          <div className="flex justify-center items-center h-svh">
+            <div className="flex flex-col justify-center items-center text-center p-8 rounded-lg bg-gray-800">
+              <h1 className="text-2xl text-white mb-4">Grocery List App</h1>
+              <button
+                onClick={signInWithGoogle}
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-xl text-lg flex items-center justify-center gap-2"
               >
-                <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z" />
-              </svg>
-              Sign in with Google
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z" />
+                </svg>
+                Sign in with Google
+              </button>
+            </div>
           </div>
         )}
       </div>
